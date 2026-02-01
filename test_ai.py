1690540ea585311808569f16562126afcdb11595
nishant160406 <chaudharinishant523@gmail.com>
2026-02-01 17:36:42 +0530

    added voice recognition

--- test_ai.py
+++ test_ai.py
@@ -1,10 +1,10 @@
+from voice_recognition import listen_from_mic
 from ai_engine import evaluate_soft_skills
-from gtts import gTTS
 from tts_utils import speak_text
-text = "I think maybe I am good at teamwork because I sometimes help others."
 
-result = evaluate_soft_skills(text)
-print(result)
-audio_file = speak_text(result["feedback"])
+spoken_text = listen_from_mic()
 
-print("Audio generated:", audio_file)
+if spoken_text:
+    result = evaluate_soft_skills(spoken_text)
+    print(result)
+    speak_text(result["feedback"])
